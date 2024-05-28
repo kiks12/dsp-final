c1a64d16fb34404f29f9e756749cc40160685d72
Francis James Tolentino <francistolentino1107@gmail.com>
2024-05-28 12:23:21 +0800

    default color theme set to green

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 from views import MainWindow
 from controllers import MainController
+import customtkinter
+
+customtkinter.set_default_color_theme("green")
 
 main_controller = MainController.MainController()
 main_window = MainWindow.MainWindow(main_controller)
